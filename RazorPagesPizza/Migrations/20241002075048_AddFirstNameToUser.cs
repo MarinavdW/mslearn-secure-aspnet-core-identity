4109636d0d5de654e0605ab60d3f7624a249dd73
marina.vanderwalt.co.za <marina.vanderwalt@fullstack.co.za>
2024-10-02 09:32:43 +0000

    Finished

---
+++ RazorPagesPizza/Migrations/20241002075048_AddFirstNameToUser.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RazorPagesPizza.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFirstNameToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FirtsName",
+                table: "AspNetUsers",
+                newName: "FirstName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FirstName",
+                table: "AspNetUsers",
+                newName: "FirtsName");
+        }
+    }
+}
